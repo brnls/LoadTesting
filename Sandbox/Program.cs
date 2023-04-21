b15eeace2b3c982850353cd35c726eb810f4b6b0
brnls <brnls1234@gmail.com>
2023-04-20 20:16:02 -0700

    init

---
+++ Sandbox/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using Dapper;
+using Npgsql;
+using System.Runtime.InteropServices;
+
+//dotnet counters monitor -p (Get-Process Sandbox).Id
+Console.WriteLine($"Processor {Environment.ProcessorCount}");
+ThreadPool.GetMinThreads(out var worker, out var completion);
+ThreadPool.SetMaxThreads(21, 1);
+Console.WriteLine($"worker {worker} - completion {completion}");
+int min = int.Parse(Console.ReadLine()!);
+ThreadPool.SetMinThreads(min,completion);
+//await Task.WhenAll(Enumerable.Range(0, 1000).Select(Sql));
+await Task.WhenAll(Enumerable.Range(0, 1000).Select(i => Task.Run(() => ThreadBusy(i))));
+
+async static Task ThreadBusy(int i)
+{
+    Thread.Sleep(3000);
+    Task.Delay(3000).Wait();
+    Console.WriteLine($"Finished {i} on {Thread.CurrentThread.ManagedThreadId}");
+}
+
+async static Task Sql(int i)
+{
+    var connectionString = "Server=localhost;Port=5432;Database=postgres;User Id=user;Password=password;" + "" +
+        "Maximum Pool Size=10;Timeout=0;Application Name=Dotnet Sandbox";
+    using var conn = new NpgsqlConnection(connectionString);
+    var sql = "select pg_sleep(3);";
+    var result = await conn.ExecuteAsync(sql);
+    Console.WriteLine($"Finished {i}");
+}
+
+public class Test
+{
+    public int id { get; set; }
+    public int dataA { get; set; }
+    public int dataB { get; set; }
+}
